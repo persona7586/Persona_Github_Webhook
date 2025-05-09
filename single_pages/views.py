d36ac25c1089994cf94a9913552782a1891aac9a
장태문 <rhffkfk@naver.com>
2025-05-09 14:15:15 +0900

    📝대문 페이지, 자기소개 페이지 만들기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+"""
+def 함수명(전달값):
+    실행 문장
+    return 반환값
+"""
+
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html'
+    )
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
